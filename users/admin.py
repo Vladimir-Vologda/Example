2979a83e640b9bb299a594b424f38efc7cebaebd
Vladimir-Vologda <vologda2018@inbox.ru>
2022-02-26 20:31:39 +0300

    Creating forms.py and admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from users.forms import CustomUserCreateForm, CustomUserChangeFormInAdmin
+from users.models import CustomUserModel
+
+
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeFormInAdmin
+    add_form = CustomUserCreateForm
+
+    list_display = ('name', 'date_birth', 'is_active', 'is_admin', 'slug')
+    list_display_links = ('name', 'slug')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('date_birth', 'slug', 'is_active')}),
+        ('Personal info', {'fields': ('name',)}),
+        ('Permission', {'fields': ('is_admin',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('name', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('name',)
+    ordering = ('name',)
+    filter_horizontal = ()
+
+
+admin.site.register(CustomUserModel, CustomUserAdmin)
+admin.site.unregister(Group)
---
+++ users/forms.py
@@ -0,0 +1,43 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+from users.models import CustomUserModel
+
+
+class CustomUserCreateForm(forms.ModelForm):
+
+    password1 = forms.CharField(label=_('Password'), widget=forms.PasswordInput)
+    password2 = forms.CharField(label=_('Password confirmation'), widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUserModel
+        fields = ('name',)
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if password1 and password2 and password1 != password2:
+            raise ValidationError(_("Password don't match"))
+        return password2
+
+    def save(self, commit=True):
+        user = super(CustomUserCreateForm, self).save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+
+        if commit:
+            user.save()
+        return user
+
+
+class CustomUserChangeFormInAdmin(forms.ModelForm):
+
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = CustomUserModel
+        fields = (
+            'name', 'date_birth', 'slug', 'is_active', 'is_admin',
+        )
